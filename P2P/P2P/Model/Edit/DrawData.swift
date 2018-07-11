1f5e11800beba0a14e60f548d399373c2a42b8fb
liamgam <liam.liamgamble@gmail.com>
2018-07-11 20:19:14 +0300

    TODO: Handle unsave pointer from Double

--- P2P/P2P/Model/Edit/DrawData.swift
+++ P2P/P2P/Model/Edit/DrawData.swift
@@ -6,14 +6,32 @@
 //  Copyright © 2018 Roma Babajanyan. All rights reserved.
 //
 
+// TODO: Handle unsave pointer from Double
+
 import Foundation
+import UIKit
+
+struct DData{
+    var x: Double
+    var y: Double
+    var state: UIGestureRecognizerState
+}
 
 class DrawData{
     var x: Double?
     var y: Double?
     
-    func initWithGestureState() -> DrawData{
+    func initWithGestureState(state: UIGestureRecognizerState, point: CGPoint) -> DrawData{
         
+        let data : NSMutableData?
+        
+        self.x = Double(point.x) // Size of Double and UIGestureRecognizerState are 8 bytes each
+        self.y = Double(point.y)
+        
+        //let xPointer = UnsafePointer<Double>
+        
+        data?.append(&x, count: MemoryLayout<Double>.size(ofValue: self.x!))
+        //data?.append(UnsafeBufferPointer<Double>.)
         
         return self
     }
